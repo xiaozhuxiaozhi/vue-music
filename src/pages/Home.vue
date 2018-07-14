e6faa42f235eae88762ba6e2a83463396b14f93d
朱自伟 <2085188234@qq.comgit config --global user.email 2085188234@qq.com>
2018-07-15 07:56:29 +0800

    修改travis配置文件路径11

---
+++ src/pages/Home.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+  <div class="container">
+    <Header />
+    <Search />
+    <SwiperSlider />
+    <MusicNav />
+    <router-view></router-view>
+    <Singer />
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header'
+import MusicNav from "./MusicNav"
+import Singer from "./Singer"
+import SwiperSlider from "../components/SwiperSilder"
+import Search from "../components/Search"
+export default {
+  name:"home",
+  data(){
+    return{
+    }
+  },
+  components:{
+    Header,
+    MusicNav,
+    Singer,
+    SwiperSlider,
+    Search
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/pages/Recommend.vue
@@ -0,0 +1,18 @@
+<template>
+	<div><MusicList musictype="2"/></div>
+</template>
+<script type="text/javascript">
+import MusicList from "../components/MusicList"
+export default {
+	name:"recommend",
+    data(){
+    	return{
+
+    	}
+	},
+	components:{
+		MusicList
+	}
+}
+</script>
+<style type="text/css" scoped></style>
